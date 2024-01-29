2fca6620f8492adc7129307ce96040f9af5bf62c
Nikita <nikitapecenigovskij4@gmail.com>
2024-01-29 03:54:52 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from validator import is_valid
+
+
+balanced_code1 = '(((([{}]))))'
+balanced_code2 = '[([])((([[[]]])))]{()}'
+balanced_code3 = '{{[()]}}'
+
+
+unbalanced_code1 = '}{}'
+unbalanced_code2 = '{{[(])]}}'
+unbalanced_code3 = '[[{())}]'
+
+if __name__ == '__main__':
+    # print(check_string(input('Введите строку')))
+    print(is_valid(balanced_code1))
+    print(is_valid(balanced_code2))
+    print(is_valid(balanced_code3))
+    print(is_valid(unbalanced_code1))
+    print(is_valid(unbalanced_code2))
+    print(is_valid(unbalanced_code3))
